01452b158198474b34de104ffd882f9732960146
danielleroux <danielleroux@users.noreply.github.com>
2022-10-20 14:31:46 +0000

    Deploying to gh-pages from @ danielleroux/ix@59fe25fb5ef0f0dc17f5ddce45252fff41db0776 🚀

---
+++ webcomponent-examples/dist/assets/ix-modal_2.entry.bed6e790.js
@@ -0,0 +1,9 @@
+import { M, a } from "./modal-8ed87a6c.54faa357.js";
+import "./index.6533181e.js";
+import "./anime.es-185e9087.87a18bcc.js";
+import "./animation-b667a4c4.18fe34bf.js";
+import "./typed-event-dd6c83dd.77656892.js";
+export {
+  M as ix_modal,
+  a as ix_modal_container
+};
---
+++ webcomponent-examples/dist/assets/toast.10fe409b.js
@@ -0,0 +1,18 @@
+import "./index.6533181e.js";
+import { t as toast } from "./index.52e24910.js";
+import "./init.2977fa8d.js";
+import "./logical-filter-operator-1bf83315.1b3b3530.js";
+import "./flip-tile-state-28a1f8ce.23318fa9.js";
+import "./modal-8ed87a6c.54faa357.js";
+import "./typed-event-dd6c83dd.77656892.js";
+import "./anime.es-185e9087.87a18bcc.js";
+import "./animation-b667a4c4.18fe34bf.js";
+import "./upload-file-state-631bb8a2.5434751f.js";
+(async function() {
+  await window.customElements.whenDefined("ix-toast-container");
+  document.getElementById("toastButton").addEventListener("click", () => {
+    toast({
+      message: "My toast message!"
+    });
+  });
+})();
