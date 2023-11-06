c2483310f28989596186a7805b5a8103b714e14b
danielleroux <leroux.daniel@googlemail.com>
2023-11-06 13:21:29 +0000

    Deploying to gh-pages from @ danielleroux/ix@a6e67c56d4c84ad044ad3ba7b7f2ed9ab72fad5f 🚀

---
+++ webcomponent-examples/dist/assets/index.6ed6a085.js
@@ -0,0 +1,5 @@
+import { a as applyPolyfills, d as defineCustomElements } from "./index.ddb8d71d.js";
+(async () => {
+  await applyPolyfills();
+  await defineCustomElements();
+})();
---
+++ webcomponent-examples/dist/assets/map-navigation-overlay.fc084bfd.js
@@ -0,0 +1,14 @@
+import "./index.ddb8d71d.js";
+/* empty css                  */import "./init.9bc89f71.js";
+const mapNav = document.querySelector("ix-map-navigation");
+const buttonOpenOverlay = document.getElementById("btn-open-overlay");
+buttonOpenOverlay.addEventListener("click", () => {
+  const overlay = document.createElement("ix-map-navigation-overlay");
+  overlay.slot = "overlay";
+  overlay.name = "Custom overlay";
+  overlay.icon = "bulb";
+  mapNav.appendChild(overlay);
+  overlay.addEventListener("closeClick", (e) => {
+    overlay.parentNode.removeChild(overlay);
+  });
+});
