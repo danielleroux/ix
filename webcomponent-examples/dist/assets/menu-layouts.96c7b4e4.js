51d00caa5cd933ffe289935e3e241c20332cd0bc
danielleroux <leroux.daniel@googlemail.com>
2023-11-06 12:38:01 +0000

    Deploying to gh-pages from @ danielleroux/ix@f3b04b7498a5e0992a50852df9226b4ba29e20a1 🚀

---
+++ webcomponent-examples/dist/assets/menu-layouts.96c7b4e4.js
@@ -0,0 +1,13 @@
+import "./index.ca2eefaa.js";
+import "./init.c2bb2f41.js";
+(async () => {
+  await window.customElements.whenDefined("ix-basic-navigation");
+  const nav = document.querySelector("ix-basic-navigation");
+  nav.breakpoints = ["md"];
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  radioButtons.forEach(
+    (rb) => rb.addEventListener("change", (event) => {
+      nav.breakpoints = [event.target.value];
+    })
+  );
+})();
