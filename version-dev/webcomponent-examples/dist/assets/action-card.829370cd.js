99693b50eca3e35e04e8fe524140253e1570ce9d
danielleroux <leroux.daniel@googlemail.com>
2023-11-06 15:15:15 +0000

    Deploying to gh-pages from @ danielleroux/ix@47f6a765ed1966dec3cbf4626b905d680ba4ea63 🚀

--- version-dev/webcomponent-examples/dist/assets/action-card.829370cd.js
+++
@@ -1,7 +0,0 @@
-import "./index.ddb8d71d.js";
-import "./init.9bc89f71.js";
-(async () => {
-  await window.customElements.whenDefined("ix-action-card");
-  const pushCardElement = document.querySelector("ix-action-card");
-  pushCardElement.addEventListener("click", console.log);
-})();
