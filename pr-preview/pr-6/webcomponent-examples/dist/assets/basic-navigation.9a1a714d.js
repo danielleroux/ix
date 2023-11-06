335fe7764aef8f3a3491252002f0392f28a87b15
danielleroux <danielleroux@users.noreply.github.com>
2023-11-06 12:21:25 +0000

    Deploy preview for PR 6 🛫

---
+++ pr-preview/pr-6/webcomponent-examples/dist/assets/basic-navigation.9a1a714d.js
@@ -0,0 +1,7 @@
+import "./index.ca2eefaa.js";
+import "./init.c2bb2f41.js";
+(async () => {
+  await window.customElements.whenDefined("ix-menu");
+  const menu = document.querySelector("ix-menu");
+  menu.breakpoints = ["medium"];
+})();
