2508f96858f54735560982d623f63f3e339dd794
danielleroux <leroux.daniel@googlemail.com>
2023-11-07 08:28:41 +0000

    Deploying to gh-pages from @ danielleroux/ix@e6f52a9c559fb051c52dc373f1879dc3de56f08b 🚀

---
+++ version-dev/webcomponent-examples/dist/assets/base-icon-button-f2514396.b9fa8c95.js
@@ -0,0 +1,8 @@
+import { h } from "./index.0fa3c693.js";
+import { B as BaseButton } from "./base-button-1ad498c0.bf53b8bd.js";
+function BaseIconButton(props) {
+  return h(BaseButton, Object.assign({}, props, { type: "button" }));
+}
+export {
+  BaseIconButton as B
+};
---
+++ version-dev/webcomponent-examples/dist/assets/basic-navigation-without-header.2199af44.js
@@ -0,0 +1,7 @@
+import "./index.0fa3c693.js";
+import "./init.f7b04142.js";
+(async () => {
+  await window.customElements.whenDefined("ix-menu");
+  const menu = document.querySelector("ix-menu");
+  menu.breakpoints = ["medium"];
+})();
